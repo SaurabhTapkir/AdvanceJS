7d61868a8aaf64c18fb73fe420c6867a959c5691
SaurabhTapkir <144579093+SaurabhTapkir@users.noreply.github.com>
2023-09-13 21:35:08 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+import {circle ,triangle,square} from './shape.js';
+
+import {default as tg} from './shapes/circle.js';
+
+
+circle();
+triangle();
+square();
+tg();
+
+/**
+ * in app.js we import all these three files from shape.js 
+ * and shape.js coantain files.
+ * 
+ */
---
+++ shape.js
@@ -0,0 +1,9 @@
+
+/**we use export bcoz we want to call these all files or function in app.js
+   this file will redirect to app.js
+   we can also use import in shape .js file and call circle and other function.
+**/
+export {circle} from './shapes/circle.js';
+export {triangle} from './shapes/triangle.js';
+
+export {square} from './shapes/square.js';
